48581bbd997baa1ff6032f4e2d94db02d772a54e
Davi-hub <53444813+Davi-hub@users.noreply.github.com>
2023-03-26 15:18:48 +0200

    pages

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -17,19 +17,19 @@ export class ProjectsComponent {
     {
       name: 'CardMaker',
       description: 'With this project you could make your own Hungarian Playing Card Deck. I used Firebase authentication and db, style made by Angular Material and pure CSS.',
-      imageUrl: '../../assets/cm.png',
+      imageUrl: '/assets/cm.png',
       detailsUrl: 'https://cardmaker-1d2fe.web.app/'
     },
     {
       name: 'My own page',
       description: 'This smaller project is this webpage, where I try introduce myself and my projects. I used Angular framework, for design Angular Material, Bootstrap (at Courses component) and pure CSS.',
-      imageUrl: '../../assets/pd.png',
+      imageUrl: '/assets/pd.png',
       detailsUrl: 'project2'
     },
     {
       name: 'Project 3',
       description: 'This is the third project',
-      imageUrl: '../../assets/cm.png',
+      imageUrl: '/assets/cm.png',
       detailsUrl: 'project3'
     }
   ];

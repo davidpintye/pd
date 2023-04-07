29e59737515ded5f569a631a5157e4c5135bf85e
Davi-hub <53444813+Davi-hub@users.noreply.github.com>
2023-04-07 13:34:03 +0200

    21546174

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'PD';
+
+  ngOnInit(): void {
+  }
 }

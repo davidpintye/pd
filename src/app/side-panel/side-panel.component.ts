36b8d08dbdec4e0e234487983c2fb7fdb54f1e7f
davidpintye <53444813+davidpintye@users.noreply.github.com>
2023-11-08 14:45:58 +0100

    3517852

--- src/app/side-panel/side-panel.component.ts
+++ src/app/side-panel/side-panel.component.ts
@@ -13,18 +13,18 @@ export class SidePanelComponent {
   @Input() isEng!: boolean;
   langSubscription = new Subscription();
   skills: Skill[] = [
-    new Skill('Angular', 'assets/skills/angular-icon-1.svg', 70),
-    new Skill('HTML5', 'assets/skills/html-1.svg', 70),
-    new Skill('CSS', 'assets/skills/css-3.svg', 65),
-    new Skill('JavaScript', 'assets/skills/logo-javascript.svg', 70),
-    new Skill('TypeScript', 'assets/skills/typescript.svg', 70),
-    new Skill('Git', 'assets/skills/git-icon.svg', 55),
-    new Skill('Java', 'assets/skills/javae.svg', 50),
+    new Skill('Angular', 'assets/skills/angular-icon-1.svg', 90),
+    new Skill('HTML5', 'assets/skills/html-1.svg', 95),
+    new Skill('CSS', 'assets/skills/css-3.svg', 80),
+    new Skill('JavaScript', 'assets/skills/logo-javascript.svg', 85),
+    new Skill('TypeScript', 'assets/skills/typescript.svg', 85),
+    new Skill('Git', 'assets/skills/git-icon.svg', 65),
+    new Skill('Java', 'assets/skills/javae.svg', 55),
     new Skill('Spring Boot', 'assets/skills/spring_boot.png', 40),
-    new Skill('Node.js', 'assets/skills/nodejs-1.svg', 45),
-    new Skill('Bootstrap', 'assets/skills/bootstrap-4.svg', 50),
-    new Skill('Firebase Auth & Db', 'assets/skills/firebase-1.svg', 45),
+    new Skill('Node.js', 'assets/skills/nodejs-1.svg', 50),
+    new Skill('Bootstrap', 'assets/skills/bootstrap-4.svg', 75),
+    new Skill('Firebase Auth & Db', 'assets/skills/firebase-1.svg', 50),
+    new Skill('MongoDB', 'assets/skills/mongodb-icon-1.svg', 50),
     new Skill('Python', 'assets/skills/python-5.svg', 25),
-    new Skill('MongoDB', 'assets/skills/mongodb-icon-1.svg', 25),
   ];
 }
